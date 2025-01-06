817e02125903d21ef6df298367c6c2a451b31617
= <=>
2025-01-06 17:33:17 +0530

    test

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -13,6 +13,15 @@ export default async function handler(req, res) {
                 pass: process.env.EMAIL_PASS,
             },
         });
+        res.status(500).json({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: true, // Use SSL (465) or TLS (587)
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        })
         try {
             await transporter.sendMail({
                 from: `"${name}" <${email}>`,
